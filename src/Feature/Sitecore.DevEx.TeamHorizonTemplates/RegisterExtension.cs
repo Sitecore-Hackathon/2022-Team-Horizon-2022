c56dfc80b9d3c7b081eec092c875a2a9b0c24f88
Julius A <julius.bichage@gmail.com>
2022-03-05 15:49:59 +0000

    Added Sitecore.DevEx dependencies

---
+++ src/Feature/Sitecore.DevEx.TeamHorizonTemplates/RegisterExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Devex.Client.Cli.Extensibility;
+using Sitecore.Devex.Client.Cli.Extensibility.Subcommands;
+using System;
+using System.Collections.Generic;
+
+namespace Sitecore.DevEx.TeamHorizonTemplates
+{
+    public class RegisterExtension : ISitecoreCliExtension
+    {
+        public IEnumerable<ISubcommand> AddCommands(IServiceProvider container)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddConfiguration(IConfigurationBuilder configBuilder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddServices(IServiceCollection serviceCollection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
